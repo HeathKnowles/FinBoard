5412f80a5a31aba7d65098e2d8065b62ae062aba
HeathKnowles <yyashuday13@gmail.com>
2025-12-02 22:48:23 +0530

    Added the provider and hooks for managing redux store

---
+++ store/index.ts
@@ -0,0 +1,35 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import layoutReducer from "@/store/layoutSlice"
+import widgetConfigReducer from "./widgetConfigSlice";
+import apiManagerReducer from "./apiManagerSlice";
+import dashboardReducer from "./dashboardSlice";
+import themeReducer from "./themeSlice";
+import { loadState, saveState } from "./persist";
+
+let preloadedState: any = undefined;
+
+if (typeof window !== "undefined") {
+  preloadedState = loadState();
+}
+
+const rootReducer = combineReducers({
+  layout: layoutReducer,
+  widgetConfigs: widgetConfigReducer,
+  api: apiManagerReducer,
+  dashboard: dashboardReducer,
+  theme: themeReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  preloadedState,
+});
+
+if (typeof window !== "undefined") {
+  store.subscribe(() => {
+    saveState(store.getState());
+  });
+}
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
